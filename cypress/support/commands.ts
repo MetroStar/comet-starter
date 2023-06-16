6a64e2f6163e7695057d00e3b5b303ab25384861
Jonathan Bouder <jbouder@metrostar.com>
2023-06-16 12:41:26 -0400

    Cleanup comments in cypress files.

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,15 +1,4 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
 
 // Sign-in command
 Cypress.Commands.add('signIn', (username, password) => {
@@ -18,18 +7,6 @@ Cypress.Commands.add('signIn', (username, password) => {
   cy.get('#submit').click();
 });
 
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
 declare global {
   namespace Cypress {
     interface Chainable {
