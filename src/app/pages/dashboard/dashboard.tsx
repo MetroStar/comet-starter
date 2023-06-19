b7232f1fcd1d7970cb68bdadabf2b11b8754c7d2
Jonathan Bouder <jbouder@metrostar.com>
2023-06-19 15:04:33 -0400

    Updates to dashboard table.

--- src/app/pages/dashboard/dashboard.tsx
+++ src/app/pages/dashboard/dashboard.tsx
@@ -1,15 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Table } from '@metrostar/comet-uswds';
+import { Table, TableColumn } from '@metrostar/comet-uswds';
 import { Spinner } from '@metrostar/comet-extras';
 import useAuth from '../../hooks/useAuth';
 import useApi from '../../hooks/useApi';
 import { Launch } from '../../api/types';
 
+interface LaunchData {
+  id: SortableDataCell;
+  name: SortableDataCell;
+  status: SortableDataCell;
+  last_updated: SortableDataCell;
+}
+
+interface SortableDataCell {
+  value: string | ReactNode;
+  sortValue: string;
+}
+
 export const Dashboard = (): React.ReactElement => {
   const { isSignedIn } = useAuth();
   const { getItems, items, loading } = useApi();
-  const [data, setData] = useState<Launch[] | null>(null);
+  const [data, setData] = useState<LaunchData[] | null>(null);
 
   useEffect(() => {
     if (isSignedIn) {
@@ -19,9 +31,33 @@ export const Dashboard = (): React.ReactElement => {
 
   useEffect(() => {
     if (items) {
-      setData(items);
+      const newData: LaunchData[] = [];
+      items.forEach((item: Launch) => {
+        newData.push({
+          name: {
+            value: (
+              <NavLink id={`details-link-${item.id}`} to={`/details/${item.id}`}>
+                {item.name}
+              </NavLink>
+            ),
+            sortValue: item.status.name,
+          },
+          id: { value: item.id, sortValue: item.id },
+          status: { value: item.status.name, sortValue: item.status.name },
+          last_updated: { value: item.last_updated, sortValue: item.last_updated },
+        });
+      });
+      setData(newData);
     }
   }, [items]);
+
+  const columns: TableColumn[] = [
+    { id: 'name', name: 'Name' },
+    { id: 'id', name: 'ID' },
+    { id: 'status', name: 'Status' },
+    { id: 'last_updated', name: 'Last Updated' },
+  ];
+
   return (
     <div className="grid-container">
       <div className="grid-row">
@@ -38,20 +74,9 @@ export const Dashboard = (): React.ReactElement => {
               <Table
                 id="characters-table"
                 className="width-full"
-                columns={[
-                  { id: 'id', name: 'ID' },
-                  { id: 'name', name: 'Name' },
-                ]}
-                data={data.map(({ id, name }) => {
-                  return {
-                    id,
-                    name: (
-                      <NavLink id={`details-link-${id}`} to={`/details/${id}`}>
-                        {name}
-                      </NavLink>
-                    ),
-                  };
-                })}
+                columns={columns}
+                data={data}
+                sortable
               />
             )
           )}
