46127d3780b401243788a5ed8e9422caf311b7e2
Johnny Bouder <jbouder@metrostar.com>
2023-09-06 18:04:07 -0400

    Update dashboard table with new Data Table.

--- src/pages/dashboard/dashboard-table/dashboard-table.tsx
+++ src/pages/dashboard/dashboard-table/dashboard-table.tsx
@@ -1,4 +1,5 @@
-import { Table, TableColumn } from '@metrostar/comet-uswds';
+import { DataTable } from '@metrostar/comet-extras';
+import { ColumnDef } from '@tanstack/react-table';
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Launch } from '../../../types/launch';
@@ -12,52 +13,61 @@ export const DashboardTable = ({
   items,
 }: DashboardTableProps): React.ReactElement => {
   const [data, setData] = useState<LaunchData[]>();
+  const cols = React.useMemo<ColumnDef<LaunchData>[]>(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Name',
+        cell: (info) => info.getValue(),
+      },
+      {
+        accessorKey: 'provider',
+        header: 'Service Provider',
+        cell: (info) => info.getValue(),
+      },
+      {
+        accessorKey: 'status',
+        header: 'Status',
+        cell: (info) => info.getValue(),
+      },
+      {
+        accessorKey: 'last_updated',
+        header: 'Last Updated',
+        cell: (info) => info.getValue(),
+      },
+    ],
+    [],
+  );
+
   useEffect(() => {
     if (items) {
       const newData: LaunchData[] = [];
       items.forEach((item: Launch) => {
         newData.push({
-          name: {
-            value: (
-              <NavLink
-                id={`details-link-${item.id}`}
-                to={`/details/${item.id}`}
-              >
-                {item.name}
-              </NavLink>
-            ),
-            sortValue: item.status.name,
-          },
-          provider: {
-            value: item.launch_service_provider.name,
-            sortValue: item.launch_service_provider.name,
-          },
-          status: { value: item.status.name, sortValue: item.status.name },
-          last_updated: {
-            value: item.last_updated,
-            sortValue: item.last_updated,
-          },
+          name: (
+            <NavLink id={`details-link-${item.id}`} to={`/details/${item.id}`}>
+              {item.name}
+            </NavLink>
+          ),
+          provider: item.launch_service_provider.name,
+          status: item.status.name,
+          last_updated: item.last_updated,
         });
       });
       setData(newData);
     }
   }, [items]);
 
-  const columns: TableColumn[] = [
-    { id: 'name', name: 'Name' },
-    { id: 'provider', name: 'Service Provider' },
-    { id: 'status', name: 'Status' },
-    { id: 'last_updated', name: 'Last Updated' },
-  ];
-
   return data ? (
-    <Table
+    <DataTable
       id="launch-table"
       className="width-full"
-      columns={columns}
+      columns={cols}
       data={data}
       sortable
-    />
+      sortCol="last_updated"
+      sortDir="desc"
+    ></DataTable>
   ) : (
     <></>
   );
