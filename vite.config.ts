e5dcd742755c7c68f4bf90c418635edb654c1142
Johnny Bouder <jbouder@metrostar.com>
2023-10-10 17:14:34 -0400

    Revert workflow node version to 18. Update vite config to manually provide __dirname for workflow.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import eslint from 'vite-plugin-eslint';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
