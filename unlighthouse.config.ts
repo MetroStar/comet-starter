c1d2d9c07bd85ccf4417238beb0c4fa4d378d896
Johnny Bouder <jbouder@metrostar.com>
2023-10-31 16:43:49 -0400

    Updates to unlighthouse config. Add workflow action.

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'unlighthouse';
 
 export default defineConfig({
-  // auth: {
-  //   username: '',
-  //   password: '',
-  // },
   ci: {
     budget: {
       performance: 90,
@@ -14,14 +10,31 @@ export default defineConfig({
     },
   },
   debug: true,
-  routerPrefix: '/comet-starter',
+  // hooks: {
+  //   async authenticate(page) {
+  //     // login to the page
+  //     await page.goto('http://some-auth-provider');
+  //     const emailInput = await page.$('input[type="text"]');
+  //     await emailInput?.type('some-user');
+  //     const passwordInput = await page.$('input[type="password"]');
+  //     await passwordInput?.type('some-password');
+  //     await Promise.all([
+  //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //       page.$eval('#login-form', (form: any) => form.submit()),
+  //       page.waitForNavigation(),
+  //     ]);
+  //   },
+  // },
   puppeteerClusterOptions: {
     maxConcurrency: 1,
+  },
+  puppeteerOptions: {
+    // headless: false,
+    // slowMo: 50,
   },
   scanner: {
     device: 'desktop',
     skipJavascript: false,
   },
-  site: 'http://localhost:8080',
-  // urls: ['/', '/signin', '/dashboard'],
+  site: 'https://metrostar.github.io/comet-starter/',
 });
