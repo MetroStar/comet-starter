f24757ac52f9c0f054b1c7c1fc6903dce3142399
Johnny Bouder <jbouder@metrostar.com>
2024-07-27 11:52:05 -0400

    Add modal use example.

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,28 @@
-import { Alert } from '@metrostar/comet-uswds';
+import { Alert, Modal } from '@metrostar/comet-uswds';
 import { getDisplayName } from '@src/utils/auth';
-import React from 'react';
+import modal from '@uswds/uswds/js/usa-modal';
+import React, { useEffect } from 'react';
 import useAuth from '../../hooks/use-auth';
 
 export const Home = (): React.ReactElement => {
   const { isSignedIn, currentUserData } = useAuth();
+
+  const handleContinue = (): void => {
+    console.log('Continue clicked...');
+    // Do something
+
+    /* eslint-disable-next-line */
+    modal.toggleModal.call(modal, false); // Manually toggle modal after work complete
+  };
+
+  // Have to add event listeners after components load due to the way the modal works
+  useEffect(() => {
+    const button = document.querySelector('#continueBtn') as HTMLButtonElement;
+    if (button) {
+      button.onclick = handleContinue;
+    }
+  }, []);
+
   return (
     <div className="grid-container">
       <div className="grid-row">
@@ -21,6 +39,25 @@ export const Home = (): React.ReactElement => {
             <Alert id="sign-in-alert" type="info">
               You are not currently signed in. Please Sign In to access the
               Dashboard.
+              <a
+                href={`#modal-1`}
+                className="usa-button"
+                aria-controls="modal-1"
+                data-open-modal
+              >
+                Sign In
+              </a>
+              <Modal
+                id="modal-1"
+                heading="Sign In Required"
+                footer={
+                  <button type="button" className="usa-button" id="continueBtn">
+                    Continue
+                  </button>
+                }
+              >
+                Click Continue to sign in.
+              </Modal>
             </Alert>
           </div>
         </div>
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,2 @@
 declare module '@uswds/uswds/js/usa-header';
+declare module '@uswds/uswds/js/usa-modal';
