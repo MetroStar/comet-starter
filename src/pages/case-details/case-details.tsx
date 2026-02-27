ff5e23907861fe4f7103ebe0f59571da0a5a6b2f
Johnny Bouder <jbouder@metrostar.com>
2026-02-27 12:07:56 -0500

    Use stronger regex.

--- src/pages/case-details/case-details.tsx
+++ src/pages/case-details/case-details.tsx
@@ -49,7 +49,7 @@ export const CaseDetails = (): React.ReactElement => {
     email: z
       .string()
       .regex(
-        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
         'Please enter a valid email address.',
       )
       .optional()
